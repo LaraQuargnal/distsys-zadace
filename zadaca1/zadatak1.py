b226b11a0b85fe5c3a48a6a30071f3b8a2bace77
LaraQuargnal <116004169+LaraQuargnal@users.noreply.github.com>
2023-01-13 12:11:25 +0100

    Update and rename zadatak1 to zadatak1.py

--- zadaca1/zadatak1.py
+++ zadaca1/zadatak1.py
@@ -2,3 +2,5 @@ def funkcija1(x):
     if not all(isinstance(y, str) for y in x):
         raise ValueError("Svi elementi u listi moraju biti string!")
     return [y for y in x if len(y) > 4]
+
+print(funkcija1(["Pas", "Macka", "Stol"]))
